bc2b05decdc80fb5788f2968218eb6a56c54b75b
KengoWada <louiskengo3@gmail.com>
2018-09-20 22:37:42 +0300

    Added comments to JS files.

--- JS/factorial.js
+++ JS/factorial.js
@@ -6,27 +6,30 @@ function factorial(n) {
 	//base value
 	else if ( n === 0 ) {
 		return 1;
-	} else {
-	//recursion
+    }
+    //recursion
+    else {
 		return n * factorial( n - 1 );
 	}
 }
 
 $(document).ready(function(){
-    $('#inputNumber').click(function(event){
-        event.preventDefault();
-        var number_value = $('#number').val();
-        var final_answer = factorial(number_value);
+    $('#inputNumber').click(function(event){ // Listens for a click on the button with Id 'inputNumber'.
+        event.preventDefault(); // Prevents the page from reloading everytime the button is clicked.
+        var number_value = $('#number').val(); // Get the value from the user input.
+        var final_answer = factorial(number_value); // Run the factorial function with user input.
         var output = [];
         var n=1;
         $('#lists').empty();
         $('#final_answer').empty();
 
+        // For loop to print every call of the recursive function.
         for (var x=number_value;x>1; x--){
             n*=x;
             $('#lists').append('<li>'+n+'</li>');
         }
 
+        // Print the final value of the recursive function.
         $('#final_answer').append(final_answer);
     });
 });
